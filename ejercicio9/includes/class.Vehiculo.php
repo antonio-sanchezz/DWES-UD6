a0fcb42c3a97aba3ffc404d6faf87150aea0c897
Antonio Sanchez Espinosa <antonio.sanchez.espinosa.al@iespoligonosur.org>
2022-01-16 18:11:18 +0100

    completado

--- ejercicio9/includes/class.Vehiculo.php
+++ ejercicio9/includes/class.Vehiculo.php
@@ -17,7 +17,7 @@ abstract class Vehiculo implements IVehiculo {
         echo "Peso: " . $obj->peso . "<br>";
         echo "Cambios de color: " . self::$numeroCambioColor . "<br>";
 
-        if (get_class($obj) == "CuatroRuedas" || get_class($obj) == "Coche") {
+        if (get_class($obj) == "CuatroRuedas") {
             echo "Número de puertas: " . $obj->numeroPuertas . "<br>";
         }
         if (get_class($obj) == "Coche") {
